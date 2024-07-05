c3d5bf2270bb14fb61787546ebd4e4402f7dc69b
EVERTON KOPEC <ekopec@paypal.com>
2024-07-05 11:18:39 -0300

    Adjustments

--- clh/logging_manager_roundtrip.go
+++ clh/logging_manager_roundtrip.go
@@ -3,7 +3,6 @@ package clh
 import (
 	"fmt"
 	"github.com/sirupsen/logrus"
-	"log"
 	"net/http"
 )
 
@@ -14,11 +13,10 @@ type LoggingRoundTripper struct {
 }
 
 func (lrt *LoggingRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	var logger *log.Logger
 	fmt.Printf("RoundTrip %s %+v\n\n\n", lrt.AppName, req)
 	if lrt.LM.ShouldLog(lrt.AppName) {
 		fmt.Printf("Request: %s %s\n\n\n", req.Method, req.URL.String())
-		logger.Printf("%v", logrus.Fields{
+		fmt.Printf("%v", logrus.Fields{
 			"request":     req.Method,
 			"app_name":    lrt.AppName,
 			"request_url": req.URL.String(),
@@ -30,7 +28,7 @@ func (lrt *LoggingRoundTripper) RoundTrip(req *http.Request) (*http.Response, er
 	}
 	if lrt.LM.ShouldLog(lrt.AppName) && resp != nil {
 		fmt.Printf("Response: %d\n\n", resp.StatusCode)
-		logger.Printf("%v", logrus.Fields{
+		fmt.Printf("%v", logrus.Fields{
 			"Response":      resp.Status,
 			"status_code":   resp.StatusCode,
 			"app_name":      lrt.AppName,
